992a5c23e73e775ac155599502140d0871418d9e
Henner4746 <henner@test.com>
2026-08-16 20:19:46 +0200

    chore(branding): update mod icon to distinct avionics HUD branding

---
+++ src/main/kotlin/ru/octol1ttle/flightassistant/api/util/extensions/FAGuiGraphics.kt
@@ -0,0 +1,7 @@
+package ru.octol1ttle.flightassistant.api.util.extensions
+
+//? if >=26.2 {
+typealias FAGuiGraphics = net.minecraft.client.gui.GuiGraphicsExtractor
+//?} else {
+typealias FAGuiGraphics = net.minecraft.client.gui.GuiGraphics
+//?}
